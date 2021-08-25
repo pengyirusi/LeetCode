61d21648bfb240e92c2655b2583a0caab165f97f
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-08-25 20:25:20 +0800

    7点多下班

---
+++ leetcode/src/S797.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Create by weiyupeng on 2021/8/25 20:12
+ */
+public class S797 {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        int num = graph.length;
+        boolean[] vis = new boolean[num];
+        List<Integer> path = new ArrayList<>();
+        path.add(0);
+        dfs(graph, 0, num - 1, vis, path);
+        return res;
+    }
+
+    private void dfs(int[][] graph, int start, int end, boolean[] vis, List<Integer> path) {
+        if (start == end) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        vis[start] = true;
+        for (int next : graph[start]) {
+            if (vis[next]) {
+                continue;
+            }
+            path.add(next);
+            dfs(graph, next, end, vis, path);
+            path.remove(path.size() - 1);
+        }
+        vis[start] = false;
+    }
+}
