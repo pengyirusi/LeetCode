cbe5ba055b91b73c92b96ee3688e08df0df2b828
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-08-26 23:21:26 +0800

    没看到这句 “每艘船最多可同时载两人” 真蠢

---
+++ leetcode/src/S881.scala
@@ -0,0 +1,19 @@
+/**
+ * Create by weiyupeng on 2021/8/26 22:57
+ */
+object S881 {
+    def numRescueBoats(people: Array[Int], limit: Int): Int = {
+        val sorted: Array[Int] = people.sorted
+        var i = 0
+        var j = sorted.length - 1
+        var res = 0
+        while (i <= j) {
+            if (sorted(i) + sorted(j) <= limit) {
+                i += 1
+            }
+            j -= 1
+            res += 1
+        }
+        res
+    }
+}
