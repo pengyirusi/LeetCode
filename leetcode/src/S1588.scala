426cfaf8cb58b8708d32e25fef80e760d6b6c0ae
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-08-29 09:16:46 +0800

    0.0

---
+++ leetcode/src/S1588.scala
@@ -0,0 +1,23 @@
+/**
+ * Create by weiyupeng on 2021/8/29 9:07
+ */
+object S1588 {
+    def sumOddLengthSubarrays(arr: Array[Int]): Int = {
+        val len: Int = arr.length
+        for (i <- 1 until len) {
+            arr(i) = arr(i) + arr(i - 1)
+        }
+        var res: Int = arr(len - 1)
+        for (i <- 3 to len
+             if i%2 == 1) {
+            for (j <- i - 1 until len) {
+                var left = 0
+                if (j - i >= 0) {
+                    left = arr(j - i)
+                }
+                res += arr(j) - left
+            }
+        }
+        res
+    }
+}
