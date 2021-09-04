ee93bfe723281cc2a84495b9808b6f9534386a0b
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-09-04 08:54:28 +0800

    0.0

---
+++ leetcode/src/SJZ10_1.scala
@@ -0,0 +1,19 @@
+/**
+ * Create by weiyupeng on 2021/9/4 8:33
+ */
+object SJZ10_1 {
+    def fib(n: Int): Int = {
+        if (n < 2) {
+            return n
+        }
+        var a = 0
+        var b = 1
+        var c = 1
+        for (_ <- 2 until n) {
+            a = b
+            b = c
+            c = (a + b) % 1000000007
+        }
+        c
+    }
+}
