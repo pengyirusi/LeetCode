146c19b3d89878641e84a3a8a2d20e2a22c5f943
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-10-05 14:30:43 +0800

    回北京啦

---
+++ leetcode/src/S284.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Create by weiyupeng on 2021/10/5 14:17
+ */
+class S284 implements Iterator<Integer> {
+
+    private int size = 0;
+    private int cur = -1;
+    private List<Integer> list;
+
+    public S284(Iterator<Integer> iterator) {
+        // initialize any member here.
+        list = new ArrayList<>();
+        while (iterator.hasNext()) {
+            list.add(iterator.next());
+        }
+        size = list.size();
+        cur = 0;
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    public Integer peek() {
+        return list.get(cur);
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    @Override
+    public Integer next() {
+        Integer ret = list.get(cur);
+        cur++;
+        return ret;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cur != -1 && cur < size;
+    }
+}
