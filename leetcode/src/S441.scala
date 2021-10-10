32d32446019cfb1740776305124111b7f3028a6f
weiyupeng1 <weiyupeng1@xiaomi.com>
2021-10-10 16:10:16 +0800

    0.0

---
+++ leetcode/src/S441.scala
@@ -0,0 +1,17 @@
+/**
+ * Create by weiyupeng on 2021/10/10 15:54
+ */
+object S441 {
+    def arrangeCoins(n: Int): Int = {
+        var step = 1
+        var all: Int = n
+        while (true) {
+            all -= step
+            if (all < 0) {
+                return step
+            }
+            step += 1
+        }
+        step
+    }
+}
